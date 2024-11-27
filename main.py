48198eebb20faed57c7cbbad35e3a00ffe1362d8
Ar74ur <arthur.almeidas@upe.br>
2024-11-27 20:57:59 -0300

    Tratamento de exceção para relações

--- main.py
+++ main.py
@@ -41,17 +41,46 @@ def parse_relations(table_relations):
         relations.append({"name": relation_name, "type": relation_type, "table": related_table, "back_name": None})
     return relations
 
-
 def parse_table(table):
     table_name = table.tableName.text
+    table_count = 0
 
     if not table_name:
         raise Exception("A tabela não possui nome")
+    for current_table_name, _ in tables.items():
+        if current_table_name == table_name:
+            table_count += 1
+        if table_count > 1:
+            raise Exception("Já existe uma tabela com esse nome")
 
     columns = parse_columns(table.column())
     relations = parse_relations(table.relation())
 
     return {"columns": columns, "relations": relations}
+
+def check_inverse_relation():
+    for table_name, table_data in tables.items():
+        for relation in table_data["relations"]:
+            related_table_name = relation["table"]
+            inverse_relation_name = ""
+
+            if relation["type"] == "one-to-many":
+                inverse_relation_name = "many-to-one"
+            elif relation["type"] == "many-to-one":
+                inverse_relation_name = "one-to-many"
+
+            if inverse_relation_name != "":
+                inverse_relation_found = False
+                for related_relation in tables[related_table_name]["relations"]:
+                    if related_relation["type"] == inverse_relation_name and related_relation["table"] == table_name:
+                        relation["back_name"] = related_relation["name"]
+                        inverse_relation_found = True
+                        break
+
+                if not inverse_relation_found:
+                    raise Exception(
+                        f"A tabela {related_table_name} não possui uma relação inversa do tipo {inverse_relation_name} para {table_name}, necessária como inversa da relação '{relation['name']}' em {table_name}"
+                    )
 
 def parse_input(input_text):
     input_stream = InputStream(input_text)
@@ -59,7 +88,7 @@ def parse_input(input_text):
     token_stream = CommonTokenStream(lexer)
     parser = DatabaseModelParser(token_stream)
     tree = parser.database()
-    if parser.getNumberOfSyntaxErrors()>0:
+    if parser.getNumberOfSyntaxErrors() > 0:
         print("O código tem um erro sintático")
         sys.exit(1)
 
@@ -67,20 +96,7 @@ def parse_input(input_text):
         table_name = table.tableName.text
         tables[table_name] = parse_table(table)
     
-    for table_name, table_data in tables.items():
-        for relation in table_data["relations"]:
-            related_table_name = relation["table"]
-
-            if relation["type"] == "one-to-many":
-                for related_relation in tables[related_table_name]["relations"]:
-                    if related_relation["type"] == "many-to-one" and related_relation["table"] == table_name:
-                        relation["back_name"] = related_relation["name"]
-                        break
-            elif relation["type"] == "many-to-one":
-                for related_relation in tables[related_table_name]["relations"]:
-                    if related_relation["type"] == "one-to-many" and related_relation["table"] == table_name:
-                        relation["back_name"] = related_relation["name"]
-                        break
+    check_inverse_relation()
 
     return tables
 
